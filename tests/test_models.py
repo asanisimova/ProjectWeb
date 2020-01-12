4ba73648d181c1f2a4ffdb01cdc049a7dd03bcba
asanisimova <31139444+asanisimova@users.noreply.github.com>
2020-01-12 18:36:12 +0300

    Add files via upload

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,18 +1,16 @@
-from django.contrib.auth import get_user_model
 from  django.test import TestCase
 
 from products.models import Product
 
 
-User = get_user_model()
-
-
 class ProductTest(TestCase):
     def setUp(self):
-        Product.objects.create(name='ТераФлю лесные ягоды пакетики 10 шт.', price=315.00)
-        Product.objects.create(name='Шалфей таблетки для рассасывания 20 шт.', price=214.00)
+        Product.objects.create(name='ТераФлю лесные ягоды пакетики 10 шт.', is_active=False, price=315.00)
+        Product.objects.create(name='Шалфей таблетки для рассасывания 20 шт.', is_active=True, price=214.00)
+        Product.objects.create(name='Шалфей таблетки для рассасывания 30 шт.', is_active=True, price=214.00)
 
-    def test_product_filtering(self):
-        product = Product.objects.filter(price=315.00)
-        print(product.count())
-        self.assertEqual(product.count(), 1)
+    def test_active_filtering(self):
+        product = Product.objects.get(name='ТераФлю лесные ягоды пакетики 10 шт.')
+        self.assertEqual(product.is_active, False)
+        products = Product.active.all()
+        self.assertEqual(products.count(), 2)
